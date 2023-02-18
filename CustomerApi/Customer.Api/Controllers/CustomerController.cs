12a211bd6ee77816530807b81e0dc3756f675230
Gelson Chyrstiano <gelson_costa@compuletra.com>
2023-02-18 01:27:12 -0300

    feat(confituration): Create Controller and update libs.

---
+++ CustomerApi/Customer.Api/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Customer.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(Guid))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> CreateCustomer()
+        {
+            return Ok();
+        }
+
+
+        [HttpGet("/delivery-addres/cep")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> CreateCustomer([FromQuery]string cep)
+        {
+            return Ok();
+        }
+    }
+}
