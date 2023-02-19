5d10a679e0a967c705517f29b2662d64676d8ee9
Gelson Chyrstiano <gelson_costa@compuletra.com>
2023-02-19 02:03:06 -0300

    feat(customer repository): implement methods.

--- CustomerApi/Customer.Infra/Repositories/CustomerRepository.cs
+++ CustomerApi/Customer.Infra/Repositories/CustomerRepository.cs
@@ -1,34 +1,46 @@
 ﻿using Customer.Domain.Aggregates;
 using Customer.Domain.DomainObjects;
 using Customer.Domain.Interfaces.Repository;
+using Customer.Infra.Context;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Customer.Infra.Repositories
 {
     public class CustomerRepository : ICustomerRepository
     {
-        public Task<Guid> CreateAsync(Person person)
+        private readonly CustomerContext _customerContext;
+
+        public CustomerRepository(CustomerContext customerContext)
         {
-            throw new NotImplementedException();
+            _customerContext = customerContext;
         }
 
-        public Task<Guid> CreateAsync(Company company)
+        public async Task<Guid> CreateAsync(Person person)
         {
-            throw new NotImplementedException();
+            _customerContext.Persons.Add(person);
+            await _customerContext.SaveChangesAsync();
+
+            return person.Id;
         }
 
-        public Task<Company> GetCompanyAsync(Cnpj cnpj)
+        public async Task<Guid> CreateAsync(Company company)
         {
-            throw new NotImplementedException();
+            _customerContext.Companies.Add(company);
+            await _customerContext.SaveChangesAsync();
+
+            return company.Id;
         }
 
-        public Task<Person> GetPersonAsync(Cpf cpf)
+        public async Task<Company> GetCompanyAsync(Cnpj cnpj)
         {
-            throw new NotImplementedException();
+            return await _customerContext.Companies.FirstOrDefaultAsync(company => company.Cnpj == cnpj);
+        }
+
+        public async Task<Person> GetPersonAsync(Cpf cpf)
+        {
+            return await _customerContext.Persons.FirstOrDefaultAsync(person => person.Cpf == cpf);
         }
     }
 }
