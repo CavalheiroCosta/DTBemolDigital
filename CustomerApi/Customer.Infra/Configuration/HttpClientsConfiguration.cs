7abdb392e9d6b5b847f25e82f8adecda2c4956da
Gelson Chyrstiano <gelson_costa@compuletra.com>
2023-02-19 21:03:59 -0300

    feat(retry): Added Polly for retry request when viaCepApi return some error.

--- CustomerApi/Customer.Infra/Configuration/HttpClientsConfiguration.cs
+++ CustomerApi/Customer.Infra/Configuration/HttpClientsConfiguration.cs
@@ -2,7 +2,10 @@
 using Customer.Infra.HttpClients.ViaCep;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Polly.Contrib.WaitAndRetry;
 using Refit;
+using System.Net;
 
 namespace Customer.Infra.Configuration
 {
@@ -10,13 +13,29 @@ namespace Customer.Infra.Configuration
     {
         public static void ConfigureHttpClients(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddRefitClient<IViaCepClient>().ConfigureHttpClient(c => c.BaseAddress = new Uri(GetClientUri(configuration, "ViaCepApi")));
+            var viaCepUrl = GetClientUri(configuration, "ViaCepApi");
+            var delay = Backoff.DecorrelatedJitterBackoffV2(medianFirstRetryDelay: TimeSpan.FromSeconds(1), retryCount: 5);
+            services.AddHttpClient(nameof(IViaCepClient), client => client.BaseAddress = new Uri(viaCepUrl))
+                            .AddPolicyHandler(GetRetryPolicy(delay))
+                            .AddTypedClient(Refit.RestService.For<IViaCepClient>);
         }
 
         private static string GetClientUri(IConfiguration configuration, string api)
         {
             var url = configuration[api];
             return string.IsNullOrEmpty(url) ? throw new ArgumentNullException(nameof(configuration), ConfigurationErrorMessages.ApiConfigurationNotFound(api)) : url;
+        }
+
+        private static Func<IServiceProvider, HttpRequestMessage, IAsyncPolicy<HttpResponseMessage>> GetRetryPolicy(IEnumerable<TimeSpan> delay)
+        {
+            return (provider, msg) =>
+            {
+                return Policy.HandleResult<HttpResponseMessage>(message =>
+                            message.StatusCode == HttpStatusCode.RequestTimeout ||
+                            message.StatusCode == HttpStatusCode.GatewayTimeout ||
+                            message.StatusCode == HttpStatusCode.InternalServerError)
+                        .WaitAndRetryAsync(delay);
+            };
         }
     }
 }
